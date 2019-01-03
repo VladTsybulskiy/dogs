0a6aef654683488c8e03ffe1fac13aa34443958f
Vladislav Tsybulskiy <vltsybulskiy@gmail.com>
2019-01-03 23:21:35 +0300

    added redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -3,4 +3,6 @@ import {createStore} from 'redux'
 import { rootReducer} from "../reducers";
 
 
-export let store=createStore(rootReducer);
+export let store=createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
